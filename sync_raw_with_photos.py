636bba5580964ae545f906d3df541dcd6873c653
Adam Lathan <lathan.adam@gmail.com>
2024-11-30 09:18:09 -0700

    Enhancing logic

--- sync_raw_with_photos.py
+++ sync_raw_with_photos.py
@@ -4,14 +4,15 @@ import os
 from os.path import exists
 
 def main():
-    folder = 'Ralph Ln STR Listing'
+    folder = '/home/adam/Pictures/2024-06-14 PNW/5D'
     raw_dir = 'RAW'
     jpg_dir = 'PHOTO'
 
     i = 0
-    for raw_file in glob(f'c:/users/alathan/pictures/{folder}/{raw_dir}/*.CR2'):
+    for raw_file in glob(f'{folder}/{raw_dir}/*.CR2'):
         if not exists(re.sub(raw_dir, jpg_dir, raw_file).replace('CR2','JPG')):
             i += 1
+            print(f'Removing {raw_file}')
             os.remove(raw_file)
     
     print(f'Removed {i} files')
